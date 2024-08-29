a069e62e573b1adc9fe50257377d4d4ec4bbb756
brcls <petrick_barcelos@hotmail.com>
2024-08-29 13:48:10 -0300

    wip: commit page

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,94 @@
-use winit::event_loop::{ControlFlow, EventLoop};
-mod app; 
-use app::App;
+use gpui::*;
 
-fn  main() {
-    let event_loop = EventLoop::new().unwrap();
+struct HelloWorld {
+    text: SharedString,
+}
 
-    // ControlFlow::Poll continuously runs the event loop, even if the OS hasn't
-    // dispatched any events. This is ideal for games and similar applications.
-    event_loop.set_control_flow(ControlFlow::Poll);
+impl Render for HelloWorld {
+    fn render(&mut self, _cx: &mut ViewContext<Self>) -> impl IntoElement {
+        div()
+            .bg(rgb(0x202020))
+            .size_full()
+            .text_xl()
+            .text_color(rgb(0xffffff))
+            .children([
+                div()
+                    .w_full()
+                    .flex()
+                    .bg(rgb(0x181818))
+                    .justify_between()
+                    .items_center()
+                    .p_2()
+                    .gap_2()
+                    .children([
+                        div().w_full().rounded_xl().h_10().bg(rgb(0x202020)),
+                        div().w_full().rounded_xl().h_10().bg(rgb(0x202020)),
+                        div().w_full().rounded_xl().h_10().bg(rgb(0x202020)),
+                        div().w_full().rounded_xl().h_10().bg(rgb(0x202020)),
+                    ]),
+                div()
+                    .h_full()
+                    .m_2()
+                    .rounded_xl()
+                    .w_1_3()
+                    .p_2()
+                    .gap_2()
+                    .flex()
+                    .flex_col()
+                    .bg(rgb(0x181818))
+                    .children([
+                        div()
+                            .w_full()
+                            .flex()
+                            .text_sm()
+                            .px_4()
+                            .items_center()
+                            .rounded_xl()
+                            .h_10()
+                            .bg(rgb(0x202020))
+                            .child(format!("Hello, {}!", &self.text)),
+                        div()
+                            .w_full()
+                            .flex()
+                            .text_sm()
+                            .px_4()
+                            .items_center()
+                            .rounded_xl()
+                            .h_10()
+                            .bg(rgb(0x202020))
+                            .child(format!("Hello, {}!", &self.text)),
+                        div()
+                            .w_full()
+                            .flex()
+                            .text_sm()
+                            .px_4()
+                            .items_center()
+                            .rounded_xl()
+                            .h_10()
+                            .bg(rgb(0x202020))
+                            .child(format!("Hello, {}!", &self.text)),
+                        div()
+                            .w_full()
+                            .flex()
+                            .text_sm()
+                            .px_4()
+                            .items_center()
+                            .rounded_xl()
+                            .h_10()
+                            .bg(rgb(0x202020))
+                            .child(format!("Hello, {}!", &self.text)),
+                    ]),
+            ])
+    }
+}
 
-    // ControlFlow::Wait pauses the event loop if no events are available to process.
-    // This is ideal for non-game applications that only update in response to user
-    // input, and uses significantly less power/CPU time than ControlFlow::Poll.
-    event_loop.set_control_flow(ControlFlow::Wait);
-
-    let mut app = App::default();
-    let _ = event_loop.run_app(&mut app);
+fn main() {
+    App::new().run(|cx: &mut AppContext| {
+        cx.open_window(WindowOptions::default(), |cx| {
+            cx.new_view(|_cx| HelloWorld {
+                text: "World".into(),
+            })
+        })
+        .unwrap();
+    });
 }
