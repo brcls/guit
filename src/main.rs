885b8701f72d8fd54717eecdfee795841fffee79
brcls <petrick_barcelos@hotmail.com>
2024-08-29 17:17:00 -0300

    fixing tab

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,82 @@
 use gpui::*;
 
 struct HelloWorld {
-    text: SharedString,
+    selected_tab: SharedString,
+}
+
+impl HelloWorld {
+    fn on_click(&mut self, tab_name: SharedString) {
+        self.selected_tab = tab_name;
+    }
 }
 
 impl Render for HelloWorld {
-    fn render(&mut self, _cx: &mut ViewContext<Self>) -> impl IntoElement {
+    fn render(&mut self, cx: &mut ViewContext<Self>) -> impl IntoElement {
+        let selected_tab = &self.selected_tab;
+
+        let tab_content = match selected_tab.as_ref() {
+            "commit" => div()
+                .h_full()
+                .m_2()
+                .rounded_xl()
+                .w_1_3()
+                .p_2()
+                .gap_2()
+                .flex()
+                .flex_col()
+                .bg(rgb(0x181818))
+                .children([
+                    div()
+                        .w_full()
+                        .flex()
+                        .text_sm()
+                        .px_4()
+                        .items_center()
+                        .rounded_xl()
+                        .h_10()
+                        .bg(rgb(0x202020))
+                        .child("file.rs")
+                        .hover(|style| style.bg(rgb(0x282828)).cursor_pointer()),
+                    div()
+                        .w_full()
+                        .flex()
+                        .text_sm()
+                        .px_4()
+                        .items_center()
+                        .rounded_xl()
+                        .h_10()
+                        .bg(rgb(0x202020))
+                        .child("file.rs")
+                        .hover(|style| style.bg(rgb(0x282828)).cursor_pointer()),
+                    div()
+                        .w_full()
+                        .flex()
+                        .text_sm()
+                        .px_4()
+                        .items_center()
+                        .rounded_xl()
+                        .h_10()
+                        .bg(rgb(0x202020))
+                        .child("file.rs")
+                        .hover(|style| style.bg(rgb(0x282828)).cursor_pointer()),
+                    div()
+                        .w_full()
+                        .flex()
+                        .text_sm()
+                        .px_4()
+                        .items_center()
+                        .rounded_xl()
+                        .h_10()
+                        .bg(rgb(0x202020))
+                        .child("file.rs")
+                        .hover(|style| style.bg(rgb(0x282828)).cursor_pointer()),
+                ]),
+            "branches" => div().child("Branches content goes here"),
+            "history" => div().child("History content goes here"),
+            "flow" => div().child("Flow content goes here"),
+            _ => div().child("Select a tab"),
+        };
+
         div()
             .bg(rgb(0x202020))
             .size_full()
@@ -31,7 +102,8 @@ impl Render for HelloWorld {
                             .rounded_xl()
                             .h_10()
                             .bg(rgb(0x202020))
-                            .child("commit"),
+                            .child("commit")
+                            .hover(|style| style.bg(rgb(0x282828)).cursor_pointer()),
                         div()
                             .w_full()
                             .flex()
@@ -42,7 +114,8 @@ impl Render for HelloWorld {
                             .rounded_xl()
                             .h_10()
                             .bg(rgb(0x202020))
-                            .child("branches"),
+                            .child("branches")
+                            .hover(|style| style.bg(rgb(0x282828)).cursor_pointer()),
                         div()
                             .w_full()
                             .flex()
@@ -53,7 +126,8 @@ impl Render for HelloWorld {
                             .rounded_xl()
                             .h_10()
                             .bg(rgb(0x202020))
-                            .child("history"),
+                            .child("history")
+                            .hover(|style| style.bg(rgb(0x282828)).cursor_pointer()),
                         div()
                             .w_full()
                             .flex()
@@ -66,58 +140,7 @@ impl Render for HelloWorld {
                             .bg(rgb(0x202020))
                             .child("flow"),
                     ]),
-                div()
-                    .h_full()
-                    .m_2()
-                    .rounded_xl()
-                    .w_1_3()
-                    .p_2()
-                    .gap_2()
-                    .flex()
-                    .flex_col()
-                    .bg(rgb(0x181818))
-                    .children([
-                        div()
-                            .w_full()
-                            .flex()
-                            .text_sm()
-                            .px_4()
-                            .items_center()
-                            .rounded_xl()
-                            .h_10()
-                            .bg(rgb(0x202020))
-                            .child("file.rs"),
-                        div()
-                            .w_full()
-                            .flex()
-                            .text_sm()
-                            .px_4()
-                            .items_center()
-                            .rounded_xl()
-                            .h_10()
-                            .bg(rgb(0x202020))
-                            .child("file.rs"),
-                        div()
-                            .w_full()
-                            .flex()
-                            .text_sm()
-                            .px_4()
-                            .items_center()
-                            .rounded_xl()
-                            .h_10()
-                            .bg(rgb(0x202020))
-                            .child("file.rs"),
-                        div()
-                            .w_full()
-                            .flex()
-                            .text_sm()
-                            .px_4()
-                            .items_center()
-                            .rounded_xl()
-                            .h_10()
-                            .bg(rgb(0x202020))
-                            .child("file.rs"),
-                    ]),
+                tab_content,
             ])
     }
 }
@@ -126,7 +149,7 @@ fn main() {
     App::new().run(|cx: &mut AppContext| {
         cx.open_window(WindowOptions::default(), |cx| {
             cx.new_view(|_cx| HelloWorld {
-                text: "World".into(),
+                selected_tab: SharedString::from("commit"),
             })
         })
         .unwrap();
